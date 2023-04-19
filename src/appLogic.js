ab7088aefcf6187346ebe6ff20faad6de2ffeab3
Martin Smeder <119725728+martinsmeder@users.noreply.github.com>
2023-04-19 18:54:32 +0200

    Added OrganizeModule

--- src/appLogic.js
+++ src/appLogic.js
@@ -1,7 +1,7 @@
 // TO DO:
-// Organize
-// An object with functions for organizing todo items by due date, priority, etc such as getting all
-// items due today or this week and sorting by priority or date
+// InterfaceHelpers
+// Exports utility functions that help with rendering the interface, such as functions for
+// creating HTML elements and attaching event listeners
 
 // Factories
 const ItemFactory = (title, description, priority, dueDate, isDone) => ({
@@ -113,4 +113,81 @@ const ProjectModule = (() => {
   };
 })();
 
-console.log(ItemModule, ProjectModule);
+const OrganizeModule = (() => {
+  // Helper functions
+  const isDueToday = (dateString) => {
+    console.log(dateString);
+    const today = new Date();
+    const date = new Date(dateString);
+    return (
+      date.getDate() === today.getDate() &&
+      date.getMonth() === today.getMonth() &&
+      date.getFullYear() === today.getFullYear()
+    );
+  };
+
+  const isDueThisWeek = (dateString) => {
+    const today = new Date();
+    const date = new Date(dateString);
+    const endOfWeek = new Date(
+      today.getFullYear(),
+      today.getMonth(),
+      today.getDate() + (7 - today.getDay())
+    );
+    return date >= today && date <= endOfWeek;
+  };
+
+  // Methods
+  const sortByDate = (items) =>
+    items.sort((a, b) => new Date(a.dueDate) - new Date(b.dueDate));
+
+  const sortByPriority = (items) => {
+    const priorities = ["high", "medium", "low"];
+    return items.sort(
+      (a, b) => priorities.indexOf(a.priority) - priorities.indexOf(b.priority)
+    );
+  };
+
+  const getAllDailyItems = () => {
+    const allDailyItems = [];
+    const allItems = [...ItemModule.itemArray];
+
+    ProjectModule.projectArray.forEach((project) => {
+      allItems.push(...project.array);
+    });
+
+    allItems.forEach((item) => {
+      if (isDueToday(item.dueDate)) {
+        allDailyItems.push(item);
+      }
+    });
+
+    return allDailyItems;
+  };
+
+  const getAllWeeklyItems = () => {
+    const allWeeklyItems = [];
+    const allItems = [...ItemModule.itemArray];
+
+    ProjectModule.projectArray.forEach((project) => {
+      allItems.push(...project.array);
+    });
+
+    allItems.forEach((item) => {
+      if (isDueThisWeek(item.dueDate)) {
+        allWeeklyItems.push(item);
+      }
+    });
+
+    return allWeeklyItems;
+  };
+
+  return {
+    getAllDailyItems,
+    getAllWeeklyItems,
+    sortByDate,
+    sortByPriority,
+  };
+})();
+
+console.log(OrganizeModule);
